680bcfcab265388c2d84d02d6d34487035f0094c
ShiGiun <107355752+Linh2k55555@users.noreply.github.com>
2026-01-26 02:48:13 +0700

    Update googleSheets.js

--- googleSheets.js
+++ googleSheets.js
@@ -1,22 +1,29 @@
 import { google } from "googleapis";
 
+const SPREADSHEET_ID = "1vaYCxcRqm5yeVa6Fxwc5wee-aHagU2hOpq39ipNfwOs";
+const SHEET_NAME = "Results";
+
+if (!process.env.GOOGLE_CREDENTIALS) {
+    throw new Error("❌ GOOGLE_CREDENTIALS chưa được set trên Render");
+}
+
 const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
+    credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
     scopes: ["https://www.googleapis.com/auth/spreadsheets"]
 });
 
 const sheets = google.sheets({ version: "v4", auth });
 
-const SPREADSHEET_ID = "1vaYCxcRqm5yeVa6Fxwc5wee-aHagU2hOpq39ipNfwOs";
-
 export async function appendExamResult(row) {
     await sheets.spreadsheets.values.append({
         spreadsheetId: SPREADSHEET_ID,
-        range: "Results",          // ⭐ CHỈ CẦN TÊN SHEET
-        valueInputOption: "RAW",
+        range: SHEET_NAME,     // ⭐ chỉ tên sheet
+        valueInputOption: "Results",
         insertDataOption: "INSERT_ROWS",
         requestBody: {
             values: [row]
         }
     });
+
+    console.log("✅ Ghi Google Sheet thành công");
 }
